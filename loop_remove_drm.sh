f0967ad70b1e2fc343c131f00f09b5b4089842bd
Anders Einar Hilden <hildenae@gmail.com>
2016-09-24 21:04:21 +0200

    Add loop_remove_drm.sh

---
+++ loop_remove_drm.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+ACTIVATION_BYTES="<add your bytes here>"
+FROM="/tmp/audible"
+TO="/tmp/audible-no-drm"
+FFMPEG="ffmpeg" # Path to ffmpeg that supports -activation_bytes
+
+COUNT_FROM="$(ls "$FROM" | wc -l)"
+
+# only aax, something fails with .aa, and i'm not up to debugging
+for FROMPATH in $FROM/*.aax; do 
+  #echo "$FROMPATH";
+  TOPATH="$TO/$(basename "$FROMPATH" | cut -d'_' -f 1).mp4"
+  if [ ! -e "$TOPATH" ]; then
+    echo "$FROMPATH => $TOPATH"
+    echo $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -vn -c:a copy "$TOPATH"
+  else
+    echo $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -vn -c:a copy "$TOPATH"
+    echo "$TOPATH found, skipping"
+  fi
+done
+
+COUNT_TO="$(ls "$TO" | wc -l)"
+
+if [ "$COUNT_FROM" -gt "$COUNT_TO" ]; then
+  echo "$COUNT_FROM sourcefiles, $COUNT_TO destination files, possible error in conversion or duplicate title";
+fi
