1a326f03c1fea75d12dbcf20018b9d7b8b1e861b
Tritian <jeffkjr@gmail.com>
2018-03-15 17:47:17 -0700

    Added support for Author Folders
    Added exiftool commands to pull Artist and Title information from the .aax files
    Used that information to enhance the path
    Fixes #7

--- loop_remove_drm.sh
+++ loop_remove_drm.sh
@@ -6,20 +6,33 @@ TO="/tmp/audible-no-drm"
 FFMPEG="ffmpeg" # Path to ffmpeg that supports -activation_bytes
 
 COUNT_FROM="$(ls "$FROM" | wc -l)"
+echo "$COUNT_FROM"
 
 # only aax, something fails with .aa, and i'm not up to debugging
-for FROMPATH in $FROM/*.aax; do 
-  #echo "$FROMPATH";
-  TOPATH="$TO/$(basename "$FROMPATH" | cut -d'_' -f 1).mp4"
+for FROMPATH in $FROM/*.aax; do
+  #Get Exif data from the file
+  ARTIST="`exiftool -m -Artist "$FROMPATH" -p '$Artist'`"
+  TITLE="`exiftool -m -Title "$FROMPATH" -p '$Title'`"
+  #Store Artist Path for mkdir
+  ARTISTPATH="$TO"/"$ARTIST"
+  #Update TOPATH to include Artist and Title data from exiftool
+  TOPATH="$ARTISTPATH"/"$TITLE".m4b
+  echo "TOPATH: $TOPATH"
+
+
   if [ ! -e "$TOPATH" ]; then
     echo "$FROMPATH => $TOPATH"
-    $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -vn -c:a copy "$TOPATH"
+    mkdir -p -- "$ARTISTPATH"
+    echo $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -c:a copy -vn -f mp4 "$TOPATH"
+    $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -c:a copy -vn -f mp4 "$TOPATH"
   else
-    echo "$TOPATH found, skipping"
+    echo $FFMPEG -loglevel error  -activation_bytes "$ACTIVATION_BYTES" -i "$FROMPATH" -c:a copy -vn -f mp4 "$TOPATH"
+    "$TOPATH found, skipping"
   fi
 done
 
-COUNT_TO="$(ls "$TO" | wc -l)"
+#Update COUNT_TO to check for files in subdirectories (Ignoring DS_Store files on MacOS)
+COUNT_TO="$(find "$TO" -type f | grep -v .DS_Store | wc -l)"
 
 if [ "$COUNT_FROM" -gt "$COUNT_TO" ]; then
   echo "$COUNT_FROM sourcefiles, $COUNT_TO destination files, possible error in conversion or duplicate title";
